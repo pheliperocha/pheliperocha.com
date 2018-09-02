5ac9cf6212fca28e892168b1c8e87d0a18a516e7
pheliperocha <phelipe.rocha@celsolisboa.edu.br>
2018-09-01 22:33:27 -0300

    feat: Page sections

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="home">
     <Header/>
+    <Section theme="light" />
+    <Section theme="dark" />
+    <Section theme="light" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Header from '@/components/Header.vue';
+import Section from '@/components/Section.vue';
 
 @Component({
   components: {
-      Header,
+    Header,
+    Section,
   },
 })
 export default class Home extends Vue {}
