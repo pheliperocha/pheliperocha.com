8fffbd2fd219f12d46673ba22bae0d7a2ab92447
pheliperocha <phelipe.rocha@celsolisboa.edu.br>
2018-09-01 22:15:18 -0300

    feat: Adding Fontawesome

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import router from './router';
 import './registerServiceWorker';
 import VueI18n from 'vue-i18n';
 import * as messages from '@/i18n';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDotCircle } from '@fortawesome/free-regular-svg-icons';
+/* tslint:disable:no-var-requires */
+const fontawesome = require('@fortawesome/vue-fontawesome');
 
 Vue.config.productionTip = false;
 
+library.add(faDotCircle);
+Vue.component('font-awesome-icon', fontawesome.FontAwesomeIcon);
 Vue.use(VueI18n);
 
 const i18n = new VueI18n({
