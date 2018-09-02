31e2e43b8756e5b718969c8dfe051429a3d789a1
pheliperocha <phelipe.rocha@celsolisboa.edu.br>
2018-09-02 12:33:58 -0300

    test: Testing Title Component

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -16,7 +16,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class Title extends Vue {
   @Prop() private value!: string;
-  @Prop() private size: number = 1;
+  @Prop() private size!: number;
 }
 </script>
 
--- tests/unit/Header.spec.ts
+++ tests/unit/Header.spec.ts
@@ -1,9 +1,17 @@
 /* tslint:disable:no-empty */
+/* tslint:disable:no-var-requires */
+
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import Header from '@/components/Header.vue';
+import Vue from 'vue';
+const fontawesome = require('@fortawesome/vue-fontawesome');
 
 describe('Header.vue', () => {
+  before(() => {
+    Vue.component('FontAwesomeIcon', fontawesome.FontAwesomeIcon);
+  });
+
   it('Should have my name', () => {
     const wrapper = shallowMount(Header, {
       mocks: {
