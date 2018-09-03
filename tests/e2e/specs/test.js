8a4537e7775a3e206631bda12569b78f5a5f565a
pheliperocha <phelipe.rocha@celsolisboa.edu.br>
2018-09-03 18:33:40 -0300

    feat: Test sections from home page

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -9,5 +9,18 @@ module.exports = {
       .assert.elementPresent('.menu')
       .assert.containsText('h1', 'Phelipe Rocha')
       .end()
-  }
-}
+  },
+
+  'Test sections from home page': browser => {
+    browser
+      .url(process.env.VUE_DEV_SERVER_URL)
+      .waitForElementVisible('#app', 5000)
+      .assert.containsText('#aboutSection h1', 'ABOUT')
+      .assert.containsText('#servicesSection h1', 'SERVICES')
+      .assert.containsText('#projectsSection h1', 'PROJECTS')
+      .assert.containsText('#blogSection h1', 'BLOG')
+      .assert.containsText('#contactSection h1', 'CONTACT')
+      .assert.containsText('#newsletterSection h1', 'NEWSLETTER')
+      .end()
+  },
+};
