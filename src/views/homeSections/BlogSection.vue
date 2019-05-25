bd97ba922dfbd17eee68140f1f801d69fd7e5d05
Phelipe Rocha <phelipeafonso@gmail.com>
2019-05-25 10:44:51 -0300

    Starting create thumb-article component

--- src/views/homeSections/BlogSection.vue
+++ src/views/homeSections/BlogSection.vue
@@ -3,9 +3,18 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12">
-                    <Title :value="$t('menu.blog')" :size="1" />
+                  <Title :value="$t('menu.blog')" :size="1" />
 
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget lorem mi. Morbi faucibus nulla non ex sodales, nec accumsan lorem hendrerit. Suspendisse sem tellus, pharetra at convallis non, vehicula quis diam. Cras leo ipsum, sodales non laoreet ac, consectetur in arcu. Donec pharetra neque nec lorem tincidunt lobortis. Nulla eu lectus dui. Nullam velit mauris, volutpat fermentum rhoncus a, aliquet at nunc. Aliquam dapibus convallis pretium. Quisque ex ante, lacinia ultricies consequat vel, sodales vel ligula. Proin mollis tellus ut ipsum volutpat, vitae feugiat diam accumsan. Morbi lobortis feugiat enim, ut congue ante ultrices ut. Phasellus eu ex sed augue cursus posuere sed eget tellus. Morbi ut finibus ex.</p>
+                  <!-- TODO: Create a component -->
+                  <figure class="thumb-article" v-on:click="openArticle('https://medium.com/hashtagdevs/a-brand-new-serverless-project-cbdb910669ae')">
+                    <img src="https://cdn-images-1.medium.com/max/2000/gradv/29/81/30/darken/25/1*0AiIklIzU2lPqK5E2nA2YQ.jpeg" alt="A Brand new Serverless Project" title="A Brand new Serverless Project">
+                    <figcaption class="title">
+                      <p class="title">A Brand new Serverless Project</p>
+                      <p class="subtitle">Serverless computing architecture for a Startup MVP.</p>
+                      <span class="date">Dec 10, 2018 • 3 min read</span>
+                    </figcaption>
+                  </figure>
+
                 </div>
             </div>
         </div>
@@ -25,7 +34,50 @@
   })
   export default class BlogSection extends Vue {
     @Prop() private theme!: string;
+
+    private openArticle(url: string): void {
+      window.open(url, '_blank');
+    }
   }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .thumb-article {
+    width: 320px;
+    height: 280px;
+    overflow: hidden;
+    border-radius: 10px;
+    position: relative;
+    cursor: pointer;
+
+    img {
+      width: 250%;
+      margin-left: -200px;
+      margin-top: -10px;
+    }
+
+    figcaption {
+      padding: 10px;
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+
+      p.title {
+        margin: 0;
+        line-height: 1.3rem;
+        margin-bottom: 8px;
+      }
+
+      p.subtitle {
+        font-size: 16px;
+        line-height: 1.3rem;
+        margin: 0;
+      }
+
+      span.date {
+        font-size: 12px;
+      }
+    }
+  }
+
+</style>
